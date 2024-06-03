d39c0f2d254da0a6ee7626f7fb56eaa576f72eee
Gabriel Leiva <gaboleiva@192.168.1.5>
2024-06-03 14:25:19 -0400

    Carga del proyecto

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://gaboleiva-dsgn.github.io',
+  base: '/portafolio',
+});
