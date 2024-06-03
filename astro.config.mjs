5fe70d64ce1f9dd12283261019534bc6efcf8bce
Gabriel Leiva <gaboleiva@192.168.1.5>
2024-06-03 17:19:07 -0400

    cambio en ruta repo para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://gaboleiva-dsgn.github.io',
-  base: '/portafolio',
+  base: '/portafolio-gabo',
 });
